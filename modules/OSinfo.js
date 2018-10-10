f798ef1695609437b83edde88690731f8b448a1e
Rafal Leszczynski <info@grafikart.pl>
2018-10-10 22:56:33 +0200

    final1.1

--- modules/OSinfo.js
+++ modules/OSinfo.js
@@ -23,27 +23,3 @@ function getOSinfo() {
 }
 
 exports.print = getOSinfo;
-
-
-
-
-// var os = require( 'os' );
-// var createTime = require( '../modules/formatTime' );
-
-// function getOSinfo() {
-//     var type = os.type();
-//     if( type === 'Darwin' ) {
-//         type = 'OSX';
-//     } else if( type === 'Windows_NT' ) {
-//         type = 'Windows';
-//     }
-//     var userInfo = os.userInfo();
-//     var release = os.release();
-//     console.log( 'User name: ', userInfo.username );
-//     console.log( 'Home dir: ', userInfo.homedir );
-//     createTime.print();
-//     console.log( 'System: ', type );
-//     console.log( 'Release: ', release );    
-// }
-
-// exports.print = getOSinfo;
